bfe9a9d4fb20640f817f40a3b3f70d60de9cce43
HF XU <hfxu@HFdeMacBook-Pro.hitronhub.home>
2022-04-02 13:02:23 -0600

    unfinished

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class SessionModel(models.Model):
+    
+    sessionid = models.TextField(max_length=200)
+
+class UserModel(models.Model):
+
+   # def user(model, userid):
+
+    userid = models.TextField(blank=False)
+    sessionid = models.ForeignKey(SessionModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+
+        return self.userid
+    
+class ActionModel(models.Model):
+    
+    userID = models.ForeignKey(user, on_delete=models.CASCADE)
+    type = models.TextField(blank=False)
+    
+    
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import HttpResponse
+import requests
+import json
+from django.core import serializers
+from models import UserModel, SessionModel, ActionModel
 
-# Create your views here.
+
+def users(request, userid):
+
+    json_userlist = []
+    users = UserModel.objects.get(userid = userid)
+    
+    with open("user.json","w") as output:
+        
+
+        for user in users:
+            action = ActionModel.objects.get(userid=userID)
+            action_dict = {
+                "time":str(datetime.now()),
+                "type":action.type
+            }
+            user_dict = {
+                "userId":user.userid,
+                "sessionId":user.sessionid,
+                "actions":action_dict
+            }
+            
+            json_userlist.append(user_dict)
+            json.dump(json_userlist, output)
+    
+   # return HttpResponse(json.dumps(json_userlist))
+
